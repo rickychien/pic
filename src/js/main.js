d4069a318cd6df6c53d3a60fe27b788725ebc22d
Ricky Chien <ricky060709@gmail.com>
2016-02-22 10:32:49 +0800

    Initialize gh-pages

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-import '../css/main.css';
-import 'font-awesome/css/font-awesome.css';
-import 'babel-core/polyfill';
-import React from 'react';
-import App from './components/app';
-
-React.render(<App/>, document.getElementById('root'));
